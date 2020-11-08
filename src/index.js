087b8ed08a97b6358000f80a5f753d020a916639
abakarovmaks <abakarovmaks@gmail.com>
2020-11-08 21:25:36 +0200

    Добавил стили

--- src/index.js
+++ src/index.js
@@ -1,51 +1,5 @@
 import './css/common.css';
-
-class CountdownTimer {
-  constructor({ targetDate }) {
-    this.targetDate = targetDate;
-    this.days = document.querySelector('.value[data-value="days"]');
-    this.hours = document.querySelector('.value[data-value="hours"]');
-    this.minutes = document.querySelector('.value[data-value="mins"]');
-    this.seconds = document.querySelector('.value[data-value="secs"]');
-    this.startTimer();
-  }
-
-  startTimer() {
-    this.getTime();
-    setInterval(() => {
-      this.getTime();
-    }, 1000);
-  }
-
-  getTime() {
-    const currentTime = Date.now();
-    const deltaTime = this.targetDate - currentTime;
-    const time = this.getTimeComponents(deltaTime);
-    this.timerInterface(time);
-  }
-
-  getTimeComponents(date) {
-    const days = this.pad(Math.floor(date / (1000 * 60 * 60 * 24)));
-    const hours = this.pad(
-      Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
-    const mins = this.pad(Math.floor((date % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = this.pad(Math.floor((date % (1000 * 60)) / 1000));
-
-    return { days, hours, mins, secs };
-  }
-
-  timerInterface(data) {
-    this.days.textContent = String(data.days);
-    this.hours.textContent = String(data.hours);
-    this.minutes.textContent = String(data.mins);
-    this.seconds.textContent = String(data.secs);
-  }
-
-  pad(value) {
-    return String(value).padStart(2, '0');
-  }
-}
+import CountdownTimer from './js/timer';
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
